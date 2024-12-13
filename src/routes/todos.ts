b0e1ba8676d062b28e1b3ce8a2a6d859306febe9
Artur Keerd <arturkeerd@gmail.com>
2024-12-13 11:48:44 +0200

    Delete the Todo by id

--- src/routes/todos.ts
+++ src/routes/todos.ts
@@ -6,6 +6,6 @@ const router = Router()
 router.post('/', createTodo)
 router.get('/', getTodos)
 router.patch('/:id', updateTodo)
-router.delete('/:id')
+router.delete('/:id', deleteTodo)
 
 export default router
