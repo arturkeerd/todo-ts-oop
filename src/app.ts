5577699dffb2017e47916ad97603a1321f631097
Artur Keerd <arturkeerd@gmail.com>
2024-12-13 11:14:39 +0200

    Create new Todo from user data

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express, { Request, Response, NextFunction} from 'express'
+import { json } from 'body-parser'
+
 import todoRoutes from './routes/todos'
 
 const app = express()
+
+app.use(json())
 
 app.use('/todos', todoRoutes)
 
@@ -9,6 +13,6 @@ app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
     res.json({message: error.message})
 })
 
-app.listen(3011, () => {
-    console.log('Server is started at http://localhost:3011')
+app.listen(3012, () => {
+    console.log('Server is started at http://localhost:3012')
 })
